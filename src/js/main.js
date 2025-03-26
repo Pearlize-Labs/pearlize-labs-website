dcc95af7a1ba3c7adb50d79956aa902a550168cf
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2025-03-26 18:39:48 +0530

    implement main carousel and about us

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 (function ($) {
     "use strict";
 
-    // Spinner
+    /**
+     * Hide the spinner after the page loads
+     */
     var spinner = function () {
         setTimeout(function () {
             if ($('#spinner').length > 0) {
@@ -11,12 +13,15 @@
     };
     spinner();
     
-    
-    // Initiate the wowjs
+    /**
+     * Initialize WOW.js for scroll animations
+     */
     new WOW().init();
 
-
-    // Sticky Navbar
+    /**
+     * Sticky Navbar
+     * Adds a sticky effect to the navbar when scrolling down
+     */
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
             $('.navbar').addClass('sticky-top shadow-sm');
@@ -24,8 +29,10 @@
             $('.navbar').removeClass('sticky-top shadow-sm');
         }
     });
-    
-    // Dropdown on mouse hover
+
+    /**
+     * Dropdown Menu on Hover (Desktop only)
+     */
     const $dropdown = $(".dropdown");
     const $dropdownToggle = $(".dropdown-toggle");
     const $dropdownMenu = $(".dropdown-menu");
@@ -34,33 +41,37 @@
     $(window).on("load resize", function() {
         if (this.matchMedia("(min-width: 992px)").matches) {
             $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
+                function() {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "true");
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function() {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "false");
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
             );
         } else {
             $dropdown.off("mouseenter mouseleave");
         }
     });
 
-
-    // Facts counter
+    /**
+     * Counter Animation
+     * Applies a counter effect to elements with the "data-toggle='counter-up'" attribute
+     */
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
         time: 2000
     });
-    
-    
-    // Back to top button
+
+    /**
+     * Back to Top Button
+     * Displays a button when scrolling down, which takes the user back to the top
+     */
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
@@ -68,13 +79,16 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
+
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
 
-
-    // Testimonials carousel
+    /**
+     * Testimonials Carousel
+     * Uses Owl Carousel to display customer testimonials in a slider format
+     */
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1500,
@@ -82,45 +96,29 @@
         loop: true,
         center: true,
         responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
+            0: { items: 1 },
+            576: { items: 1 },
+            768: { items: 2 },
+            992: { items: 3 }
         }
     });
 
-
-    // Vendor carousel
+    /**
+     * Vendor Carousel
+     * Displays vendor logos in a continuous scrolling carousel
+     */
     $('.vendor-carousel').owlCarousel({
         loop: true,
         margin: 45,
         dots: false,
-        loop: true,
         autoplay: true,
         smartSpeed: 1000,
         responsive: {
-            0:{
-                items:2
-            },
-            576:{
-                items:4
-            },
-            768:{
-                items:6
-            },
-            992:{
-                items:8
-            }
+            0: { items: 2 },
+            576: { items: 4 },
+            768: { items: 6 },
+            992: { items: 8 }
         }
     });
-    
-})(jQuery);
 
+})(jQuery);
